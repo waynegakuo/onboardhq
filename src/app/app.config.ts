becd5d62d567423193df99b23a2fa5d4690500d9
Wayne Gakuo <waynejotham@gmail.com>
2026-03-09 15:44:25 +0300

    feat(emulators): Add support for Firebase Emulator Suite in development
    - Configured emulators for Functions and Firestore in `firebase.json` and `environment.development.ts`.
    - Integrated Functions emulator setup in `app.config.ts` for streamlined local development.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,32 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
 import {environment} from '../environments/environment.development';
+import {connectFunctionsEmulator, getFunctions, provideFunctions} from '@angular/fire/functions';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideFirebaseApp(() => initializeApp(environment.firebaseConfig))
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFunctions(() => {
+      const app = initializeApp(environment.firebaseConfig);
+      const functions = getFunctions(app, 'africa-south1');
+      if (isDevMode() && environment.emulator) {
+        console.log(
+          `Connecting to Functions emulator on ${environment.emulator.host}:${environment.emulator.functionsPort}`
+        );
+        connectFunctionsEmulator(
+          functions,
+          environment.emulator.host,
+          environment.emulator.functionsPort
+        );
+      }
+      return functions;
+    }),
   ]
 };
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -8,5 +8,10 @@ export const environment = {
     messagingSenderId: "1059997354870",
     appId: "1:1059997354870:web:2f70718fc2a32dadb28857",
     measurementId: "G-9BZLELR943"
+  },
+  emulator: {
+    host: 'localhost',
+    functionsPort: 5001,
+    firestorePort: 8080
   }
 };
