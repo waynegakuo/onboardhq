8a57782a1f5914ef3204ed2b4da3545f0fba5ac9
Wayne Gakuo <waynejotham@gmail.com>
2026-03-09 09:05:50 +0300

    feat(dependencies): Add @angular/fire to project dependencies
    - Updated `package.json` and `package-lock.json` to include `@angular/fire` v20.0.1.
    - Added required dependencies and peer dependencies for Firebase integration.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {environment} from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideClientHydration(withEventReplay())
+    provideClientHydration(withEventReplay()),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig))
   ]
 };
