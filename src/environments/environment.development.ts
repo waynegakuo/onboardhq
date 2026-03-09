e1f23420a1052762a07fe015a0cf9be81d23c327
Wayne Gakuo <waynejotham@gmail.com>
2026-03-09 09:01:05 +0300

    feat(environment): Add Firebase environment configuration for development and production
    - Created `environment.development.ts` for development settings.
    - Configured `angular.json` to handle environment-specific file replacements.
    - Added `environment.ts` for production settings.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCPurjAxpjXZVTusTqQxuQ7PqICeW-kKEI",
+    authDomain: "onboarding-hq.firebaseapp.com",
+    projectId: "onboarding-hq",
+    storageBucket: "onboarding-hq.firebasestorage.app",
+    messagingSenderId: "1059997354870",
+    appId: "1:1059997354870:web:2f70718fc2a32dadb28857",
+    measurementId: "G-9BZLELR943"
+  }
+};
